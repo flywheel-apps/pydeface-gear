6a74cc178977a09ebcf60f09d29118749943e2f2
Nicolas Pannetier <nicolaspannetier@flywheel.io>
2020-02-25 14:21:11 -0800

    Running with nipype

--- run.py
+++ run.py
@@ -1,24 +1,61 @@
 #!/usr/bin/env python3
 import os
-import os.path as op
-import json
 import logging
+from nipype.interfaces.base import File, traits, CommandLineInputSpec, CommandLine
 
 from gear_toolkit import gear_toolkit_context
-from utils import args
 
 
 log = logging.getLogger(__name__)
 
 
-def main(context):
-    # Build and Execute Parameters
-    try:
-        # build the command string
-        params = args.build(context)
+class PyDefaceInputSpec(CommandLineInputSpec):
+    infile = File(exists=True,
+                  argstr='%s',
+                  position=-1,
+                  mandatory=True,
+                  desc='Path to input nifti.')
+    template = File(exists=True,
+                    argstr='--template %s',
+                    mandatory=False,
+                    desc='Optional template image that will be used as the registration ""target instead of the '
+                         'default.')
+    facemask = File(exists=True,
+                    argstr='--facemask %s',
+                    mandatory=False,
+                    desc='Optional face mask image that will be used instead of the default.')
+    cost = traits.Enum('mutualinfo', 'corratio', 'normcorr', 'normmi', 'leastsq', 'labeldiff', 'bbr',
+                       argstr='--cost %s', desc='FSL-FLIRT cost function. Default is <mutualinfo>',
+                       usedefault=True)
+    nocleanup = traits.Bool(argstr='--nocleanup',
+                            desc='Do not cleanup temporary files. Off by default.')
+    verbose = traits.Bool(argstr='--verbose',
+                          desc='Do not catch exceptions and show exception traceback (Drop into pdb debugger).')
 
-        # Execute on those parameters.
-        args.execute(context, params)
+
+class PyDeface(CommandLine):
+    _cmd = 'pydeface'
+    input_spec = PyDefaceInputSpec
+
+
+def main(context):
+    try:
+        # Instantiate node
+        pydef = PyDeface()
+
+        # Connect input node to config
+        pydef.inputs.infile = context.get_input_path('infile')
+        if context.get_input_path('template'):
+            pydef.inputs.template = context.get_input_path('template')
+        if context.get_input_path('facemask'):
+            pydef.inputs.facemask = context.get_input_path('facemask')
+        pydef.inputs.cost = context.config.get('cost')
+        pydef.inputs.nocleanup = context.config.get('nocleanup')
+        pydef.inputs.verbose = context.config.get('verbose')
+
+        # Run
+        log.info(pydef.cmdline)
+        pydef.run()
 
     except Exception as e:
         context.log.exception(e)
